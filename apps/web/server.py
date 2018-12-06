c143582889a672b09f6d8d6ed467bf950f2618ed
harikrishnan <harikrishnan.a@infibeam.net>
2018-12-06 07:00:23 +0530

    Initial commit

---
+++ apps/web/server.py
@@ -0,0 +1,36 @@
+from twisted.application import service, internet
+import os
+
+from twisted.protocols import basic
+from twisted.internet import protocol, reactor
+
+class HTTPEchoProtocol(basic.LineReceiver):
+	def __init__(self):
+		self.lines = []
+
+	def lineReceived(self, line):
+		self.lines.append(line)
+		if not line:
+			self.sendResponse()
+
+	def sendResponse(self):
+		self.sendLine('HTTP/1.1 200 OK')
+		self.sendLine('')
+		responseBody = "You said:\r\n\r\n" + "\r\n".join(self.lines)
+		self.transport.write(responseBody)
+		self.transport.loseConnection()
+
+class HTTPEchoFactory(protocol.ServerFactory):
+	def buildProtocol(self, addr):
+		return HTTPEchoProtocol()
+
+
+# default port in case of the env var not was properly set.
+PORT = 8000
+
+proxy_port = int(os.environ.get('PORT', PORT))
+
+application = service.Application('TwistedDockerized')
+factory = HTTPEchoFactory()
+server = internet.TCPServer(proxy_port, factory)
+server.setServiceParent(application)
